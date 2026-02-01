c5e45e9b6da33b10a30d83833570eef979108ff6
ToHellForever <dim.anosoff2018@yandex.ru>
2026-02-01 19:55:25 +0300

    добавил возможность добавления имени и ссылки в модель проекта

--- core/models.py
+++ core/models.py
@@ -5,11 +5,14 @@ class Project(models.Model):
         return f"Project {self.id}"
 
 class ProjectPhoto(models.Model):
+    name = models.CharField(max_length=100, help_text="Название проекта", default="")
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='photos')
     image = models.ImageField(upload_to='project_photos/')
     order = models.PositiveIntegerField(default=0)
-
+    link = models.URLField(blank=True, null=True, help_text="Ссылка на сайт", default="")
+    
     def __str__(self):
+        
         return f"Photo {self.id} for Project {self.project.id}"
 
     class Meta:
