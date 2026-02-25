5f173e9f42ce26eeb38de9be72c1e61899235a49
ToHellForever <dim.anosoff2018@yandex.ru>
2026-02-25 16:00:43 +0300

    черновой вариант политики конфиденциальности

--- g/urls.py
+++ g/urls.py
@@ -6,9 +6,14 @@ from django.conf.urls.static import static
 from core.views import LandingView, submit_application
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", LandingView.as_view(), name="landing"),
-    path('submit-application/', submit_application, name='submit_application'),
+    path(
+        "privacy-policy/",
+        LandingView.as_view(template_name="privacy_policy.html"),
+        name="privacy_policy",
+    ),
+    path("submit-application/", submit_application, name="submit_application"),
 ]
 
 
@@ -16,5 +21,5 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     ]
