aa59bda10a1d231370f16b469a5058489dd5e144
ToHellForever <dim.anosoff2018@yandex.ru>
2026-03-14 13:01:44 +0300

    .

--- g/settings.py
+++ g/settings.py
@@ -24,7 +24,6 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 TELEGRAM_BOT_API_KEY = os.getenv("TELEGRAM_BOT_API_KEY")
 TELEGRAM_USER_ID = os.getenv("TELEGRAM_USER_ID")
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
